5725bbe95d2e5df7ceeba2286412e92daee57aa1
Rairden <erik.rairden@gmail.com>
2020-10-02 00:51:55 -0500

    fixed install.sh v1

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 dir=/home/"$USER"/.config/alacritty
 dotfiles=/home/"$USER"/.dotfiles
 
-if [ ! -d "dotfiles" ]; then
+if [ ! -d "$dotfiles" ]; then
     git clone https://gitlab.com/Rairden/dotfiles.git "$dotfiles"
 fi
 
@@ -15,42 +15,42 @@ if [ ! -d "$dir" ]; then
 fi
 
 if [ ! -L ~/.gitconfig ]; then
-    ln -s dotfiles/.gitconfig    /home/"$USER"/.gitconfig
+    ln -s "$dotfiles"/.gitconfig    /home/"$USER"/.gitconfig
     echo "Symlink created:  /home/$USER/.gitconfig"
 fi
 
 if [ ! -L ~/.vimrc ]; then
-    ln -s dotfiles/.vimrc        /home/"$USER"/.vimrc
+    ln -s "$dotfiles"/.vimrc        /home/"$USER"/.vimrc
     echo "Symlink created:  /home/$USER/.vimrc"
 fi
 
 if [ ! -L ~/.zshrc ]; then
-    ln -s dotfiles/.vimrc        /home/"$USER"/.zshrc
+    ln -s "$dotfiles"/.zshrc        /home/"$USER"/.zshrc
     echo "Symlink created:  /home/$USER/.zshrc"
 fi
 
 if [ ! -L ~/.tmux.conf ]; then
-    ln -s dotfiles/.tmux.conf    /home/"$USER"/.tmux.conf
+    ln -s "$dotfiles"/.tmux.conf    /home/"$USER"/.tmux.conf
     echo "Symlink created:  /home/$USER/.tmux.conf"
 fi
 
 if [ ! -L ~/.config/alacritty/alacritty.yml ]; then
-    ln -s dotfiles/alacritty.yml     /home/"$USER"/.config/alacritty/alacritty.yml
+    ln -s "$dotfiles"/alacritty.yml     /home/"$USER"/.config/alacritty/alacritty.yml
     echo "Symlink created:  /home/$USER/.config/alacritty/alacritty.yml"
 fi
 
 if [ ! -L /root/.vimrc ]; then
-    ln -s dotfiles/.vimrc        /root/.vimrc
+    ln -s "$dotfiles"/.vimrc        /root/.vimrc
     echo "Symlink created:  /root/.vimrc"
 fi
 
 if [ ! -L /root/.zshrc ]; then
-    ln -s dotfiles/.zshrc        /root/.zshrc
+    ln -s "$dotfiles"/.zshrc        /root/.zshrc
     echo "Symlink created:  /root/.zshrc"
 fi
 
 if [ ! -L /root/.tmux.conf ]; then
-    ln -s dotfiles/.tmux.conf    /root/.tmux.conf
+    ln -s "$dotfiles"/.tmux.conf    /root/.tmux.conf
     echo "Symlink created:  /root/.tmux.conf"
 fi
 
